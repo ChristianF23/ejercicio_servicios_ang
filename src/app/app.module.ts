5a8c22648a242fd597317be5c45070e6b7fee6d0
Christian Florez <55306489+ChristianF23@users.noreply.github.com>
2021-02-08 23:58:58 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataServiciosComponent } from './data-servicios/data-servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataServiciosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/coleccion_servicios.ts
@@ -0,0 +1,19 @@
+import { Servicio } from './servicios';
+
+export const DATA_SERVICIOS : Servicio[] = [
+    {
+        nombre:  'Masaje básico',
+        descripcion: 'Masaje que se aplica en la espalda',
+        costo: 10
+    },
+    {
+        nombre:  'Masaje drenaje',
+        descripcion: 'Masaje que se aplica para drenajes',
+        costo: 15
+    },
+    {
+        nombre:  'Masaje aroma terapia',
+        descripcion: 'Masaje básico, más aromatización',
+        costo: 12
+    },
+]
